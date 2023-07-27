8b7510779046186ce59e6b6ed43cb3d5eb4a5dee
TNQT14 <2002quangthai@gmail.com>
2023-07-27 09:04:10 +0700

    curve

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:social_media/screen/widgets/curve_clipper.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -8,14 +9,17 @@ class LoginScreen extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         child: SizedBox(
-          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
           child: Column(
             children: <Widget>[
-              Image(
-                height: MediaQuery.of(context).size.height/2.5,
-                width: double.infinity,
-                image: const AssetImage('assets/images/login_background.jpg'),
-                fit: BoxFit.cover,
+              ClipPath(
+                clipper: CurveClipper(),
+                child: Image(
+                  height: MediaQuery.of(context).size.height/2.5,
+                  width: double.infinity,
+                  image: const AssetImage('assets/images/login_background.jpg'),
+                  fit: BoxFit.cover,
+                ),
               ),
               Text('FRENZY',
               style: TextStyle(
@@ -56,6 +60,47 @@ class LoginScreen extends StatelessWidget {
                     ),
                   ),
                 ),
+              ),
+              const SizedBox(height: 10,),
+              GestureDetector(
+                onTap: (){},
+                child: Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 10),
+                  alignment: Alignment.center,
+                  height: 45,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: const Text('Login',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 22,
+                    fontWeight: FontWeight.w600,
+                    letterSpacing: 1.5
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                  child: Align(
+                    alignment: FractionalOffset.bottomCenter,
+                    child: GestureDetector(
+                      onTap: (){},
+                      child: Container(
+                        alignment: Alignment.center,
+                        color: Theme.of(context).primaryColor,
+                        height: 80,
+                        child: const Text("Don't have account? Sign up?",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                        ),
+                        ),
+                      ),
+                    ),
+                  ),
               ),
             ],
           ),
